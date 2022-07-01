b2f4f731dba0b3179805eb97fb5d26ceba3dd4b2
Etiénne Lansing <Development.Github@elansing.de>
2022-07-01 22:21:10 +0200

    added Config File with first Configs

---
+++ assets/config.php
@@ -0,0 +1,15 @@
+<?php
+
+  return array(
+   'BaseURL' => 'https://www.elansing-it.de/',
+   'IP2Location-Token' => '3bd490e18169e9',
+   'Session-Token' => 'elansing_jiqSVaWughZ6ruRY7Hj7PtCAJm8vxS8NS5DkySPVNpN5ZJuN8MN6Rc8E7MEoe6My',
+   'DB' => 'elansing_Webinterface',
+   'DB-Host' => 'localhost',
+   'DB-User' => 'elansing_Webinterface',
+   'DB-Passwd' => 'UphATtEWviuXuJT#CV8CvZaGzhUcnE%zjaurC!vm^u&Uw2R9MX9RiE8NSspp##nQ',
+   'ResetPasswd' => 'true',
+   'ResetPasswd-Key' => 'CHQXn9pLjnC5EncNaW86Nf2W2wQ2moatEsHKFh6uwidSQtJRinchtBhQqDg22ohB'
+  );
+
+?>
